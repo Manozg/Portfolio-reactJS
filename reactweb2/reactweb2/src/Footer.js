6ba6bc4fc67180e08ddba11fdd848564f279ea46
Manoj Ghimire <manoj.ghimire7@gmail.com>
2020-07-20 18:24:43 -0500

    final version part1

--- reactweb2/reactweb2/src/Footer.js
+++ reactweb2/reactweb2/src/Footer.js
@@ -7,12 +7,14 @@ function Footer () {
         <React.Fragment>
         <footer className="bg-light text-center col-12 mx-auto footer">
         <div>
+            <br />
             <h3>Social Media</h3>
                 <NavLink className="btn btn-social-icon btn-instagram" to="/http://instagram.com/"><i className="fa fa-instagram" /></NavLink>{' '}
                 <NavLink className="btn btn-social-icon btn-facebook" to="/http://www.facebook.com/"><i className="fa fa-facebook" /></NavLink>{' '}
                 <NavLink className="btn btn-social-icon btn-twitter" to="/http://twitter.com/"><i className="fa fa-twitter" /></NavLink>{' '}
                 <NavLink className="btn btn-social-icon btn-google" to="/http://youtube.com/"><i className="fa fa-youtube" /></NavLink> 
         </div>
+        <br />
         </footer>
         </React.Fragment>
     )
